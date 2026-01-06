6ad8c063679124fb0b457036ea26f01f5c973be8
nicochnz <chiche.nicolas@gmail.com>
2026-01-06 10:36:50 +0100

    del: readme add: config next githubpages

--- elody_psy/next.config.ts
+++ elody_psy/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
-
+const repoName = "Elody_Psy";
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
