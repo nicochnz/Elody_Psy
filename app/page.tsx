a77be8e992f7979db831166c480efa29bf1b20f4
nicochnz <chiche.nicolas@gmail.com>
2026-01-08 10:17:06 +0100

    footer and header fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 'use client';
 import Homepage from "./pages/homepage/page";
-import Header from "./components/header";
-import Footer from "./components/footer";
 export default function Home() {
   return (
     <div >
-      <Header />
       <Homepage />
-      <Footer />
     </div>
   );
 }
